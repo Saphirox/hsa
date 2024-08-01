fcd896368274b7a7d09daa1a26d0a2494a38425c
maksymsuprunenko <suprmaks@gmail.com>
2024-08-01 15:31:20 +0300

    Added mongo and elasticsearch

---
+++ ab.bash
@@ -0,0 +1,1 @@
+ab -n 1000 -c 20 http://localhost/
--- api/server.py
+++ api/server.py
@@ -2,20 +2,42 @@
 import os
 
 from flask import Flask
+from datetime import datetime
+from elasticsearch import Elasticsearch
 from pymongo import MongoClient
+
+
 
 app = Flask(__name__)
 
-#client = MongoClient("mongo:27017")
 
 @app.route('/')
 def todo():
-    try:
-        # client.admin.command('ismaster')
-        return "Hello backwards"
-    except:
-        return "Server not available"
-    return "Hello from the MongoDB client!\n"
+    es_client = Elasticsearch(
+    ['http://elasticsearch:9200'],  # Use your Elasticsearch host
+    verify_certs=False,
+    ssl_show_warn=False  # Optional: suppresses warnings about insecure SSL
+)
+    mongo_client = MongoClient("mongo:27017")
+
+    es_client.index(index="my-index-000001", id="45", body={"any": "data", "timestamp": datetime.now()})
+    data = es_client.get(index="my-index-000001", id="45")
+
+    db = mongo_client['example_database']
+    collection = db['example_collection']
+
+    # Insert a document
+    document = {"name": "John Doe", "age": 30}
+    insert_result = collection.insert_one(document)
+    print(f"Inserted document ID: {insert_result.inserted_id}")
+
+    # Retrieve the document
+    retrieved_doc = collection.find_one({"name": "John Doe"})
+    print(f"Retrieved document: {retrieved_doc}")
+
+    mongo_client.close()
+
+    return data
 
 
 if __name__ == "__main__":
